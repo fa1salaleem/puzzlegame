c7cd732d1abc8281f73d7ad6474fe8c64f1f26ad
Muhammad Faisal Aleem <fsl.aleem@gmail.com>
2020-08-20 12:53:44 +0500

    Puzzle Game First Commit

---
+++ Puzzle/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+	public GameObject Library,InProgess,Categories,MyCollection,GamePlaySetting,SettingsPanel,DailyPuzzle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void LibraryFunction(){
+		Library.SetActive (true);
+		InProgess.SetActive (false);
+		Categories.SetActive (false);
+		MyCollection.SetActive (false);
+		GamePlaySetting.SetActive (false);
+		SettingsPanel.SetActive (false);
+		DailyPuzzle.SetActive (false);
+	}
+
+	public void DailyFunction(){
+		Library.SetActive (false);
+		InProgess.SetActive (false);
+		Categories.SetActive (false);
+		MyCollection.SetActive (false);
+		GamePlaySetting.SetActive (false);
+		SettingsPanel.SetActive (false);
+		DailyPuzzle.SetActive (true);
+	}
+
+	public void InProgessFunction(){
+		Library.SetActive (false);
+		InProgess.SetActive (true);
+		Categories.SetActive (false);
+		MyCollection.SetActive (false);
+		GamePlaySetting.SetActive (false);
+		SettingsPanel.SetActive (false);
+		DailyPuzzle.SetActive (false);
+	}
+
+	public void CategoryFunction(){
+		Library.SetActive (false);
+		InProgess.SetActive (false);
+		Categories.SetActive (true);
+		MyCollection.SetActive (false);
+		GamePlaySetting.SetActive (false);
+		SettingsPanel.SetActive (false);
+		DailyPuzzle.SetActive (false);
+	}
+
+	public void MyCollectionFunction(){
+		Library.SetActive (false);
+		InProgess.SetActive (false);
+		Categories.SetActive (false);
+		MyCollection.SetActive (true);
+		GamePlaySetting.SetActive (false);
+		SettingsPanel.SetActive (false);
+		DailyPuzzle.SetActive (false);
+	}
+
+	public void GamePlayFunction(){
+		Library.SetActive (false);
+		InProgess.SetActive (false);
+		Categories.SetActive (false);
+		MyCollection.SetActive (false);
+		GamePlaySetting.SetActive (true);
+		SettingsPanel.SetActive (false);
+		DailyPuzzle.SetActive (false);
+	}
+
+	public void Settings(){
+		SettingsPanel.SetActive (true);
+	}
+
+	public void BackFunction(){
+		if (SettingsPanel.activeSelf) {
+			SettingsPanel.SetActive (false);
+		}
+	}
+
+}
